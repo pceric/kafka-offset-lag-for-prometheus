f453ef54f1bf86409377626d2428eac0e3569e47
Eric Hokanson <hokanson@gmail.com>
2017-05-26 21:26:12 -0600

    A much more reliable way to get offsets.

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # kafka-offset-lag-for-prometheus
 Expose Kafka consumer offset lag to prometheus.
 
+## Requirements
+Kafka v0.9.0.0 or higher.  This exporter will only export offsets
+stored in Kafka.
+
 ## Getting Started
 This project is designed to run with Docker.
 `docker-compose up -d`
 Use the following ENV vars to change the default options:
-* KAFKA_BROKERS=comma separated list of brokers
-* PROMETHEUS_ADDR=address and port for Prometheus to bind to
-* REFRESH_INTERVAL=how long in seconds between each refresh
-* DEBUG=true or false
+* KAFKA_BROKERS=comma separated list of brokers (default localhost:9092)
+* PROMETHEUS_ADDR=address and port for Prometheus to bind to (default :7979)
+* REFRESH_INTERVAL=how long in seconds between each refresh (default 15)
+* DEBUG=true or false (default false)
 
 You may also build and run locally using cli arguments.  See the Dockerfile
 for build instructions.
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	kafkaBrokers   = flag.String("kafka-brokers", "localhost:9092", "Comma separated list of kafka brokers.")
-	prometheusAddr = flag.String("prometheus-addr", "localhost:7979", "Prometheus listen interface and port.")
+	prometheusAddr = flag.String("prometheus-addr", ":7979", "Prometheus listen interface and port.")
 	refreshInt     = flag.Int("refresh-interval", 15, "Time between offset refreshes in seconds.")
 	debug          = flag.Bool("debug", false, "Enable debug output.")
 )
@@ -30,11 +30,6 @@ func init() {
 }
 
 func main() {
-	type groupData struct {
-		Topic           string
-		PartitionOffset map[int32]int64
-	}
-
 	go func() {
 		config := sarama.NewConfig()
 		config.ClientID = "kafka-offset-lag-for-prometheus"
@@ -45,11 +40,42 @@ func main() {
 		}
 		defer client.Close()
 		for {
-			groupSet := make(map[string]*groupData)
+			topicSet := make(map[string]map[int32]int64)
+			time.Sleep(time.Duration(*refreshInt) * time.Second)
 			timer := prometheus.NewTimer(LookupHist)
 			client.RefreshMetadata()
-			// First lookup our group data using the metadata
+			// First grab our topics, all partiton IDs, and their offset head
+			topics, err := client.Topics()
+			if err != nil {
+				log.Printf("Error fetching topics: %s", err.Error())
+				continue
+			}
+			for _, topic := range topics {
+				// Don't include internal topics
+				if strings.HasPrefix(topic, "__") {
+					continue
+				}
+				partitions, err := client.Partitions(topic)
+				if err != nil {
+					log.Printf("Error fetching partitions: %s", err.Error())
+					continue
+				}
+				if *debug {
+					log.Printf("Found topic '%s' with %d partitions", topic, len(partitions))
+				}
+				topicSet[topic] = make(map[int32]int64)
+				for _, partition := range partitions {
+					toff, err := client.GetOffset(topic, partition, sarama.OffsetNewest)
+					if err != nil {
+						log.Printf("Problem fetching offset for topic '%s', partition '%d'", topic, partition)
+						continue
+					}
+					topicSet[topic][partition] = toff
+				}
+			}
+			// Now lookup our group data using the metadata
 			for _, broker := range client.Brokers() {
+				// Sarama plays russian roulette with the brokers
 				broker.Open(client.Config())
 				_, err := broker.Connected()
 				if err != nil {
@@ -65,42 +91,34 @@ func main() {
 				for group, t := range gresponse.Groups {
 					// we only want active consumers
 					if t == "consumer" {
-						ofrequest := new(sarama.OffsetFetchRequest)
-						ofrequest.Version = 1
-						ofrequest.ConsumerGroup = group
-						oresponse, err := broker.FetchOffset(ofrequest)
-						if err != nil {
-							log.Printf("Could not get offset: %s\n", err.Error())
-						}
-						for topic, p := range oresponse.Blocks {
-							gd := new(groupData)
-							gd.Topic = topic
-							gd.PartitionOffset = make(map[int32]int64)
-							for partition, block := range p {
-								if *debug {
-									log.Printf("Adding group: %s, topic: %s, partition: %d, offset: %d\n", group, topic, partition, block.Offset)
-								}
-								gd.PartitionOffset[partition] = block.Offset
+						// This is not very efficient but the kafka API sucks
+						for topic, data := range topicSet {
+							ofrequest := new(sarama.OffsetFetchRequest)
+							ofrequest.Version = 1
+							ofrequest.ConsumerGroup = group
+							for partition := range data {
+								ofrequest.AddPartition(topic, partition)
 							}
-							groupSet[group] = gd
+							oresponse, err := broker.FetchOffset(ofrequest)
+							if err != nil {
+								log.Printf("Could not get offset: %s\n", err.Error())
+							}
+							for _, ofrb := range oresponse.Blocks {
+								for gp, block := range ofrb {
+									if *debug {
+										log.Printf("Discovered group: %s, topic: %s, partition: %d, offset: %d\n", group, topic, gp, block.Offset)
+									}
+									if block.Offset >= 0 {
+										lag := float64(data[gp] - block.Offset)
+										OffsetLag.With(prometheus.Labels{"topic": topic, "group": group, "partition": strconv.FormatInt(int64(gp), 10)}).Set(lag)
+									}
+								}
+							}
 						}
 					}
-				}
-			}
-			// Now grab the topic data and calculate lag
-			for group, data := range groupSet {
-				for partition, offset := range data.PartitionOffset {
-					toff, err := client.GetOffset(data.Topic, partition, sarama.OffsetNewest)
-					if err != nil {
-						log.Printf("Problem fetching offset for topic '%s', partition '%d'", data.Topic, partition)
-						continue
-					}
-					lag := float64(toff - offset)
-					OffsetLag.With(prometheus.Labels{"topic": data.Topic, "group": group, "partition": strconv.FormatInt(int64(partition), 10)}).Set(lag)
 				}
 			}
 			timer.ObserveDuration()
-			time.Sleep(time.Duration(*refreshInt) * time.Second)
 		}
 	}()
 	prometheusListen(*prometheusAddr)
